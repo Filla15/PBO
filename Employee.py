644f70e4731d7c7935f240d80a811bb985ba0c62
filla15 <fillafirontina@gmail.com>
2023-06-20 09:57:42 +0700

    Membuat file latihan PBO

---
+++ Employee.py
@@ -0,0 +1,31 @@
+class Man_Employee:
+	suku = "Bugis"
+	empCount = 0
+
+	def __init__(self,name,age,agama,jnskel,alamat,status,salary):
+		self.name = name
+		self.salary = salary
+		self.name = name
+		self.age = age
+		self.jnskel = jnskel
+		self.agama = agama
+		self.alamat = alamat
+		self.status = status
+		Man_Employee.empCount +=1
+
+	def displayCount(self):
+		print("Total Employee %d" % Employee.empCount)
+
+	def displayCount(self):
+		print("Name :",self.name, ", Salary: ", self.salary)
+
+	def displayCount(self):
+		return f"Nama : {self.name} \nUsia: {self.age} \nAgama: {self.agama} \nJenis Kelamin: {self.jnskel} \nAlamat: {self.alamat} \nStatus: {self.status}"
+
+tes_qois = Man_Employee("Ageng",22,"Agama","laki-laki,"Batang","Jomblo",1000000)
+print(tes_qois.biodata())
+print("Suku: ",tes_qois.suku)
+
+tes_qois.displayEmployee()
+tes_qois.displayEmployee()
+print("total Employee %d" % Man_Employee.empCount)
---
+++ Matematika.py
@@ -0,0 +1,22 @@
+class Matematika:
+    def __init__(self,nilai1,nilai2,nilai3,nilai4):
+        self.nilai1 = nilai1
+        self.nilai2 = nilai2
+        self.nilai3 = nilai3
+        self.nilai4 = nilai4
+
+    def penjumlahan(self):
+        hasil = nilai1+nilai3
+        print (hasil)
+        return hasil
+    def perkalian(nilai1, nilai2, nilai3):
+        hasil = nilai1*nilai2*nilai3
+        print (hasil)
+        return hasil
+    def pengurangan(nilai1, nilai2, nilai3,nilai4):
+        hasil = nilai1-nilai2-nilai3-nilai4
+        print (hasil)
+        return hasil
+    
+    pengurangan(10,10,10,10)
+    perkalian(20,10,6)
