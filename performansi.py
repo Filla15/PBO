11e41051fb965d933f0350bb6e39c998343346ac
filla15 <fillafirontina@gmail.com>
2023-06-23 15:02:47 +0700

    Menambahkan File baru hr karyawan performansi dan program

---
+++ performansi.py
@@ -0,0 +1,7 @@
+class Performansi:
+    def track(self, employees, hours):
+        print('Tracking Employee Productivity')
+        print('==============================')
+        for Employee in employees:
+            employee.work(hours)
+        print('')
